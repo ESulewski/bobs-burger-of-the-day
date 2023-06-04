88b8dcb9765bc1ce0d8e483c42c51e6f004138a7
esluewski <esulewski2013@gmail.com>
2023-06-04 06:59:17 -0400

    set up displayBurger function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 function displayBurger(){
-    
-}
-
 let burgerElement = document.querySelector("#day-burger");
-burgerElement.innerHTML = displayBurger();
+
+    let burgers = [
+       "Ruta-Bag-A Burger",
+        "Romaines of the Day Burger",
+        "50 Ways to Leave Your Guava Burger",
+        "Summer Thyme Burger",
+        "Peas and Thank You Burger",
+        "Yes I Cayenne Burger"
+    ]
+    let special = burgers[Math.floor(Math.random()* burgers.length)];
+    burgerElement.innerHTML = special, burgers[random];
+}
+displayBurger();
+
